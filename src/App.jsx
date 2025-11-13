4472bf7bcd12e022a55cc06cc58961fdd64f07db
MicheleFamoso <michelefamoso@gmail.com>
2025-11-13 19:16:15 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+        eius perferendis, neque voluptatem illum ducimus veniam nam sit, fuga
+        labore earum delectus saepe doloremque ratione numquam! Facilis soluta
+        ipsam numquam. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Blanditiis cum nam veritatis porro sed odit quidem, quasi eveniet,
+        aliquam qui iure autem molestiae quibusdam nobis recusandae minus sint.
+        Reiciendis, dignissimos!
+      </p>
+    </>
+  );
+}
+
+export default App;
